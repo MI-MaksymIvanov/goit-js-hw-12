cedf85c3edb5ac4e3909038cf2fd9a55712b15ca
MI-MaksymIvanov <ivanov.maksym@gmail.com>
2025-01-24 21:31:30 +0100

    did hw12

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,17 +1,17 @@
-export function fetchImages(searchedQuery) {
-  const searchParams = new URLSearchParams({
-    key: '48264895-d0a6c310440d31e1e67da8433',
-    q: searchedQuery,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: 21,
-  });
+import axios from 'axios';
 
-  return fetch(`https://pixabay.com/api/?${searchParams}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
-}
+export const fetchImages = (searchedQuery, currentPage, perPage) => {
+  const axiosOptions = {
+    params: {
+      key: '48264895-d0a6c310440d31e1e67da8433',
+      q: searchedQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: currentPage,
+      per_page: perPage,
+    },
+  };
+
+  return axios.get(`https://pixabay.com/api/`, axiosOptions);
+};
--- src/main.js
+++ src/main.js
@@ -6,60 +6,124 @@ import SimpleLightbox from 'simplelightbox';
 const searchFormEl = document.querySelector('.js-search-form');
 const galleryEl = document.querySelector('.js-gallery');
 const loaderEl = document.querySelector('.js-loader');
+const loadMoreBtnEl = document.querySelector('.js-load-more-btn');
 
-const onSearchFormSubmit = event => {
-  event.preventDefault();
+const perPage = 15;
 
-  const searchedQuery = event.currentTarget.elements.user_query.value.trim();
+const lightbox = new SimpleLightbox('.js-gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
-  if (searchedQuery === '') {
-    iziToast.show({
-      title: 'Error:',
-      message: 'Поле має бути заповнено!!',
-      position: 'topRight',
-    });
-    return;
-  }
+let page = 1;
+let searchedQuery = '';
+let totalPages = 0;
 
-  loaderEl.classList.remove('hidden');
+const onSearchFormSubmit = async event => {
+  try {
+    event.preventDefault();
 
-  fetchImages(searchedQuery)
-    .then(data => {
-      loaderEl.classList.add('hidden');
+    searchedQuery = event.currentTarget.elements.user_query.value.trim();
 
-      if (data.hits.length === 0) {
-        iziToast.show({
-          title: 'Error:',
-          message: `За вашим запитом нічого не знайдено!`,
-          position: 'topRight',
-        });
-
-        galleryEl.innerHTML = '';
-        searchFormEl.reset();
-        return;
-      }
-
-      const galleryTemplate = data.hits
-        .map(el => createGalleryCardTemplate(el))
-        .join('');
-
-      galleryEl.innerHTML = galleryTemplate;
-      searchFormEl.reset();
-
-      const lightbox = new SimpleLightbox('.js-gallery a', {
-        captionsData: 'alt',
-        captionDelay: 250,
-      });
-      lightbox.refresh();
-    })
-    .catch(error => {
-      loaderEl.classList.add('hidden');
+    if (searchedQuery === '') {
       iziToast.show({
-        title: 'Error',
-        message: `Помилка: ${error.message}`,
+        title: 'Error:',
+        message: 'Поле має бути заповнено!!',
         position: 'topRight',
       });
+      return;
+    }
+
+    page = 1;
+
+    loadMoreBtnEl.classList.add('is-hidden');
+
+    loaderEl.classList.remove('hidden');
+
+    const { data } = await fetchImages(searchedQuery, page, perPage);
+
+    loaderEl.classList.add('hidden');
+
+    if (data.hits.length === 0) {
+      iziToast.show({
+        title: 'Error:',
+        message: `За вашим запитом нічого не знайдено!`,
+        position: 'topRight',
+      });
+
+      galleryEl.innerHTML = '';
+      searchFormEl.reset();
+      return;
+    }
+
+    totalPages = Math.ceil(data.totalHits / perPage);
+
+    if (totalPages > 1) {
+      loadMoreBtnEl.classList.remove('is-hidden');
+      loadMoreBtnEl.addEventListener('click', onLoadMoreBtnClick);
+    } else {
+      iziToast.info({
+        message: `Нажаль це всі результати за Вашим запитом: ${searchedQuery}`,
+        position: 'topRight',
+      });
+    }
+
+    const galleryTemplate = data.hits
+      .map(el => createGalleryCardTemplate(el))
+      .join('');
+
+    galleryEl.innerHTML = galleryTemplate;
+    searchFormEl.reset();
+
+    lightbox.refresh();
+  } catch (err) {
+    loaderEl.classList.add('hidden');
+    iziToast.show({
+      title: 'Error',
+      message: `Помилка: ${err.message}`,
+      position: 'topRight',
     });
+  }
 };
 
 searchFormEl.addEventListener('submit', onSearchFormSubmit);
+const onLoadMoreBtnClick = async event => {
+  try {
+    page++;
+
+    const { data } = await fetchImages(searchedQuery, page, perPage);
+
+    const galleryTemplate = data.hits
+      .map(el => createGalleryCardTemplate(el))
+      .join('');
+
+    galleryEl.insertAdjacentHTML('beforeend', galleryTemplate);
+
+    lightbox.refresh();
+
+    const firstCard = galleryEl.querySelector('.gallery-card');
+    const cardHeight = firstCard.getBoundingClientRect().height;
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+
+    if (page === totalPages) {
+      loadMoreBtnEl.classList.add('is-hidden');
+      loadMoreBtnEl.removeEventListener('click', onLoadMoreBtnClick);
+
+      iziToast.info({
+        message:
+          'We are sorry, but you have reached the end of search results.',
+        position: 'topRight',
+      });
+    }
+  } catch (err) {
+    iziToast.show({
+      title: 'Error',
+      message: `Помилка: ${err.message}`,
+      position: 'topRight',
+    });
+  }
+};
